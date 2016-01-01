7e92692e103630c7863be2e17e5eacc159308626
root <root@ravuthz-ubuntu-15.10>
2016-01-01 14:52:58 +0700

    Update Print Tag Html

--- app/Http/Controllers/Homepage/ShowController.php
+++ app/Http/Controllers/Homepage/ShowController.php
@@ -14,8 +14,8 @@ class ShowController extends Controller {
         $data['posts'] = Post::getAll();
         $data['slides'] = [
             (object) ['src' => 'assets/img/gallery/slider-img-1.jpg', 'alt' => 'Slider', 'href' => 'gallery-single.htm', 'name' => 'test1'],
-            (object) ['src' => 'assets/img/gallery/slider-img-1.jpg', 'alt' => 'Slider', 'href' => 'gallery-single.htm', 'name' => 'test1'],
-            (object) ['src' => 'assets/img/gallery/slider-img-1.jpg', 'alt' => 'Slider', 'href' => 'gallery-single.htm', 'name' => 'test1'],
+            (object) ['src' => 'assets/img/gallery/slider-img-2.jpg', 'alt' => 'Slider', 'href' => 'gallery-single.htm', 'name' => 'test1'],
+            (object) ['src' => 'assets/img/gallery/slider-img-2.jpg', 'alt' => 'Slider', 'href' => 'gallery-single.htm', 'name' => 'test1'],
             (object) ['src' => 'assets/img/gallery/slider-img-1.jpg', 'alt' => 'Slider', 'href' => 'gallery-single.htm', 'name' => 'test1']
         ];
         $data['post'] = Post::getLatest()->first();
